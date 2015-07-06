886c48763e0dd778e4c63408c1e7c235d4067e56
Natali <rinykia@gmail.com>
2015-07-06 14:11:22 +0300

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  resources :orders
+
+  resources :line_items
+
+  resources :carts
+
+  root to: 'store#index', as: 'store'
+  get 'store/index'
+
+  resources :products do
+    get :who_bought, on: :member
+  end
+
   resources :products
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
