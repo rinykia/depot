71ae9ae63dd61750349af3bc1dfa9744b791bb48
Natali <rinykia@gmail.com>
2015-07-07 13:29:33 +0300

    current curt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,17 +28,20 @@ class ApplicationController < ActionController::Base
         redirect_to login_url, notice: "Please log in"
       end
     end
+
+
+
+    private
+
+  def current_cart
+    Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+  helper_method :current_cart
 end
 
 
 
-#private
-
-  #def current_cart
-    #Cart.find(session[:cart_id])
-    #rescue ActiveRecord::RecordNotFound
-    #cart = Cart.create
-    #session[:cart_id] = cart.id
-    #cart
-  #end
-  #helper_method :current_cart
