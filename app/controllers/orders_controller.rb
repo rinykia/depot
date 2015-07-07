261fa35ff88402398159718de0860a089f4d5ba1
Natali <rinykia@gmail.com>
2015-07-07 12:38:29 +0300

    add langyage

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,7 +40,8 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver
-        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
+        format.html { redirect_to store_url, notice: 
+          I18n.t('.thanks') }
         format.json { render action: 'show', status: :created, location: @order }
       else
         @cart = current_cart
